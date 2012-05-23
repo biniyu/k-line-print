5d9c9ff37b330d9fd36c91856bc024e8635ae389
osc_zju <osc_zju@163.com>
2012-05-23 01:24:08 +0000

    导出K线数据类

---
+++ KLineWriter.cpp
@@ -0,0 +1,19 @@
+#include "StdAfx.h"
+#include "KLineWriter.h"
+#include "KLineCollection.h"
+#include <fstream>
+
+#define SZ 1024
+
+KLineWriter::KLineWriter(void)
+{
+}
+
+KLineWriter::~KLineWriter(void)
+{
+}
+
+void KLineWriter::Write(string path, KLineCollection& klines)
+{
+
+}
