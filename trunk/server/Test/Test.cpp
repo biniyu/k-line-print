8651836c040bf3068643b49553cb52d3c4aecf12
osc_zju@163.com <osc_zju@163.com@606fea6a-20f5-31eb-95c3-cff64aa984ef>
2012-08-23 13:12:16 +0000

    proactor原型

--- trunk/server/Test/Test.cpp
+++ trunk/server/Test/Test.cpp
@@ -1,4 +1,4 @@
-// Exchange.cpp : 定义控制台应用程序的入口点。
+// Test.cpp : 定义控制台应用程序的入口点。
 //
 
 #include "ace/OS.h"
@@ -6,10 +6,8 @@
 #include <ace/Thread_Manager.h>
 
 #include "ace/INET_Addr.h"
-#include "ace/SOCK_Stream.h"
-#include "ace/SOCK_Acceptor.h"
 #include "ace/Log_Msg.h"
-#include "ace/Asynch_Acceptor.h"
+#include "ace/Asynch_Connector.h"
 #include "ace/Asynch_IO.h"
 #include "ace/Proactor.h"
 #include "ace/WIN32_Proactor.h" 
@@ -39,25 +37,6 @@ private:
   ACE_Asynch_Read_Stream reader_;
   ACE_Asynch_Write_Stream writer_;
 };
-
-class Exchange_Proactive_Acceptor :
-    public ACE_Asynch_Acceptor<Exchange_Proactive_Service>
-{
-public:
-  virtual int validate_connection
-    (const ACE_Asynch_Accept::Result& result,
-     const ACE_INET_Addr &remote,
-     const ACE_INET_Addr &local);
-};
-
-int
-Exchange_Proactive_Acceptor::validate_connection (
-   const ACE_Asynch_Accept::Result&,
-   const ACE_INET_Addr& remote,
-   const ACE_INET_Addr& local)
-{
-	return 0;	
-}
 
 void
 Exchange_Proactive_Service::open (ACE_HANDLE h, ACE_Message_Block&)
@@ -126,15 +105,12 @@ Exchange_Proactive_Service::handle_write_stream
 
 int ACE_TMAIN (int, ACE_TCHAR *[])
 {
-	ACE_INET_Addr listen_addr(12345);     // Set up with listen port
+	ACE_INET_Addr peer_addr(12345, "127.0.0.1");    // Set up peer addr
 
-	Exchange_Proactive_Acceptor aio_acceptor;
-	
-	if (0 != aio_acceptor.open (listen_addr,
-								0,     // bytes_to_read
-								1))     // pass_addresses
-	  ACE_ERROR_RETURN ((LM_ERROR, ACE_TEXT ("%p\n"),
-						 ACE_TEXT ("acceptor open")), 1);
+	ACE_Asynch_Connector<Exchange_Proactive_Service> aio_connect;
+
+	aio_connect.open();
+	aio_connect.connect (peer_addr);
 
 	int success = 1;
 
@@ -145,6 +121,5 @@ int ACE_TMAIN (int, ACE_TCHAR *[])
 	}
 
 	return 0;
-
-
 }
+
