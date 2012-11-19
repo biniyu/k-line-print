86755d39d9d42ed57fec12863f2647fac06c6edc
osc_zju <osc_zju@163.com>
2012-11-19 14:13:01 +0000

    简化程序，为重构做准备

--- client/win/KLinePrint.cpp
+++ client/win/KLinePrint.cpp
@@ -85,7 +85,7 @@ BOOL CKLinePrintApp::InitInstance()
 	// 更改用于存储设置的注册表项
 	// TODO: 应适当修改该字符串，
 	// 例如修改为公司或组织名
-	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
+	SetRegistryKey(_T("Oscar"));
 	LoadStdProfileSettings(4);  // 加载标准 INI 文件选项(包括 MRU)
 
 	InitContextMenuManager();
