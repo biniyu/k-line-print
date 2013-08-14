0a9e7447ebc0bfb9d2794f85f3e5a6132bfd372c
osc_zju <osc_zju@163.com>
2013-08-14 12:51:49 +0000

    播放引擎数据结构

--- server/server.fork/svr.cpp
+++ server/server.fork/svr.cpp
@@ -7,11 +7,84 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <signal.h>
-
+#include <string>
 
 #define MAX_LISTEN 5
 #define PORT 1987
 #define IP "127.0.0.1"
+
+using namespace std;
+
+#define MAX_ACCOUNT_NUM             1000
+#define MAX_CACHE_DATA_ITEM         60
+#define MAX_SYMBOL_PER_ACCOUNT      3
+
+//  K线数据结构
+typedef struct _bar
+{
+    unsigned int date;
+    unsigned int time;
+    float   open;
+    float   high;
+    float   low;
+    float   close;
+    float   vol;    
+}Bar;
+
+//  分笔数据结构
+typedef struct _tick
+{
+    unsigned int date;
+    unsigned int time;
+    float price;
+    float vol;
+    float interest;
+    float b_price;
+    float b_vol;
+    float s_price;
+    float s_vol;    
+}Tick;
+
+typedef union _u_tick_bar
+{
+    Tick tick;
+    Bar bar;
+}UTickBar;
+
+#define DATA_TYPE_TICK  0 
+#define DATA_TYPE_BAR   1 
+
+#define BAR_TYPE_1_SECOND    0
+#define BAR_TYPE_1_MINUTE    1
+#define BAR_TYPE_5_MINUTE    2
+#define BAR_TYPE_15_MINUTE   3
+#define BAR_TYPE_30_MINUTE   4
+
+typedef struct _symbol
+{
+    bool active;        // 是否激活
+    string name;        // 合约名称
+    short datatype;     // 数据类型
+    short bartype;      // K线周期
+    short startpos;     // 数据起始位置
+    short endpos;       // 数据结束位置
+    UTickBar tickbar[MAX_CACHE_DATA_ITEM];  //  行情数据
+}Symbol;
+
+typedef struct _account
+{
+    unsigned int id;        //  帐号ID        
+    unsigned int lastdate;  //  上一日期
+    unsigned int lasttime;  //  上一时间
+    unsigned int curdate;   //  当前日期
+    unsigned int curtime;   //  当前时间
+    unsigned int speed;     //  速度
+    Symbol symbols[MAX_SYMBOL_PER_ACCOUNT];     //  合约
+    int heartbeat;          //  心跳
+}Account;
+
+//  账户数组
+Account accouts[MAX_ACCOUNT_NUM];
 
 void serve_proc(int conn_fd)
 {
