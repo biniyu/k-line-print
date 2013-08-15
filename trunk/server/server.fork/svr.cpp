b68dcd470cb8002b9eda7e232138b133256101a4
osc_zju@163.com <osc_zju@163.com@606fea6a-20f5-31eb-95c3-cff64aa984ef>
2013-08-15 14:24:55 +0000

    创建线程

--- trunk/server/server.fork/svr.cpp
+++ trunk/server/server.fork/svr.cpp
@@ -8,10 +8,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <string>
-
-#define MAX_LISTEN 5
-#define PORT 1987
-#define IP "127.0.0.1"
+#include <pthread.h>
 
 using namespace std;
 
@@ -86,95 +83,47 @@ typedef struct _account
 //  账户数组
 Account accouts[MAX_ACCOUNT_NUM];
 
-void serve_proc(int conn_fd)
+static void* controlRoutine(void* arg)
 {
-	int recv_num;
-	int send_num;
-	char recv_buf[100];
-	char send_buf[100];	
+    while(1)
+    {
+        sleep(1);
+        printf("ctl tick\n");
+    }
+}
 
-	int pid = getpid();
-
-	printf("client %d started\n", pid);
-
-	while (1) {
-		//receive
-		recv_num = recv(conn_fd, recv_buf, sizeof(recv_buf), 0);
-		if (recv_num < 0) {
-			close(conn_fd);
-			exit(1);
-		}
-		recv_buf[recv_num] = '\0';
-		printf("child proc %d got : %s\n", pid, recv_buf);
-
-		if (strcmp(recv_buf,"quit") == 0) {
-			printf("child proc %d quit\n", pid);
-			break;
-		}
-
-		//send
-		sprintf(send_buf, "server proc got %d bytes\n", recv_num);
-		send_num = send(conn_fd, send_buf, strlen(send_buf), 0);
-		if (send_num < 0) {
-			close(conn_fd);
-			exit(1);
-		}
-		printf("child proc %d sent : %s\n", pid, send_buf);
-	}
-
-	close(conn_fd);
+static void* dataRoutine(void* arg)
+{
+    while(1)
+    {
+        sleep(2);
+        printf("data tick\n");
+    }
 }
 
 int main()
 {
-	
-	signal(SIGCHLD, SIG_IGN);
-	
-	pid_t pid;
+    pthread_t controlThreadId, dataThreadId;
 
-	int conn_fd;
-	int sock_fd = socket(AF_INET,SOCK_STREAM,0);
-	if (sock_fd < 0) {
-		perror("create socket failed");
-		exit(1);
-	}
+    //  控制线程
+    pthread_create(&controlThreadId,
+                   NULL, 
+                   &controlRoutine,
+                   NULL);
+    
+    //  数据线程
+    pthread_create(&dataThreadId,
+                   NULL, 
+                   &dataRoutine,
+                   NULL);
 
-	struct sockaddr_in addr_client;
-	socklen_t client_size = sizeof(struct sockaddr_in);
-
-	struct sockaddr_in addr_serv;
-	memset(&addr_serv, 0, sizeof(addr_serv));
-	addr_serv.sin_family = AF_INET;
-	addr_serv.sin_port = htons(PORT);
-	addr_serv.sin_addr.s_addr = inet_addr(IP);
-
-	if (bind(sock_fd,(struct sockaddr *)&addr_serv,sizeof(struct sockaddr_in)) < 0) {
-		perror("bind error");
-		exit(1);
-	}
-
-	if (listen(sock_fd,MAX_LISTEN) < 0) {
-		perror("listen failed");
-		exit(1);
-	}
-
-	while (1) {
-		conn_fd = accept(sock_fd, (struct sockaddr *)&addr_client, &client_size);
-		if (conn_fd < 0) {
-			perror("accept failed");
-			exit(1);
-		}
-
-		pid = fork();
-		
-		if (pid == 0) { // child proc 
-			close(sock_fd);
-			serve_proc(conn_fd);
-		} else { // main proc
-			close(conn_fd);
-		}
-	}
-		
-	close(sock_fd);
-	return 0;
+    //  播放线程    
+    while(1)
+    {
+        //  100ms 粒度
+        usleep(100000);
+        printf("play tick\n");        
+    }
 }
+
+
