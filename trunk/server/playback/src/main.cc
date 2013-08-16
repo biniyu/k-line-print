67a62b8be62f2f632079eb6df931b6351917a46c
osc_zju@163.com <osc_zju@163.com@606fea6a-20f5-31eb-95c3-cff64aa984ef>
2013-08-16 13:41:17 +0000

    add playback engine

--- trunk/server/playback/src/main.cc
+++ trunk/server/playback/src/main.cc
@@ -1,3 +1,22 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
+/*
+ * main.cc
+ * Copyright (C) 2013 ??? ?????? <oscar@ubuntu>
+ * 
+ * playback is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * playback is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -69,9 +88,7 @@ typedef struct _symbol
 
 typedef struct _account
 {
-    unsigned int id;        //  帐号ID        
-    time_t last_time;       //  上一时间
-    int last_msec;          //    
+    unsigned int id;        //  帐号ID
     time_t cur_time;        //  当前时间
     int cur_msec;           //
     unsigned int speed;     //  速度
@@ -125,19 +142,29 @@ int main()
         for(int i = 0; i < MAX_ACCOUNT_NUM; i++)
         {
             Account& acc = accounts[i];
+			
             if(acc.heartbeat > 0)
             {
-				acc.
+				acc.cur_msec += 100 * acc.speed;
+
+				acc.cur_time += acc.cur_msec / 1000;
+				acc.cur_msec = acc.cur_msec % 1000;
 				
                 for(int idx_sym = 0; idx_sym < MAX_SYMBOL_PER_ACCOUNT; idx_sym++)
                 {
-                    
+                    Symbol& sym = acc.symbols[idx_sym];
+
+					if(sym.active)
+					{
+						
+					}
                 }
             }
         }
-
+		
         printf("play tick\n");        
     }
 }
+
 
 
